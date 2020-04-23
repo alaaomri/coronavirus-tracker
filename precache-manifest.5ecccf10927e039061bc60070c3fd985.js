8349028c9c4137f2e1a7d8f31da57d763e8aacca
omri alaa <omrialaaeddine@gmail.com>
2020-04-23 21:53:14 +0100

    Updates

---
+++ precache-manifest.5ecccf10927e039061bc60070c3fd985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d33f6174d424e7b0cfe2c150dd8043c",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "6fe39f3d231c942118fa",
+    "url": "/coronavirus-tracker/static/css/main.87834b75.chunk.css"
+  },
+  {
+    "revision": "e50c16b0c3543b0a8ccd",
+    "url": "/coronavirus-tracker/static/js/2.5d7a23b1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus-tracker/static/js/2.5d7a23b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe39f3d231c942118fa",
+    "url": "/coronavirus-tracker/static/js/main.18c3f89b.chunk.js"
+  },
+  {
+    "revision": "6fd159095b101c22a8f7",
+    "url": "/coronavirus-tracker/static/js/runtime-main.bf9473a5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronavirus-tracker/static/media/image.d7265326.png"
+  }
+]);
